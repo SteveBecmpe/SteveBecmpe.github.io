34208a22ea079dae0ef3f064b92e1a3330fde32c
Steve Black <SteveBlackecmpe@gmail.com>
2020-12-20 21:44:19 -0500

    created static html quiz structure with css hide and js buttons to show hide content.. not sure if this or todo list is correct way to do this project

---
+++ portfolio/Quiz/Quiz.js
@@ -0,0 +1,23 @@
+let QuizQuestions = {
+    // obj array : [ 0 answer choose , 1 correct answer, 2 # of buttons(questions), 3 question text, 4 h #,5 Subtext, 6 h #, 7 A1, 8 A2, 9 A3, 10 A4]
+    Q0 : [0, 0, 1, "Coding Quiz Challenge", "h1", "Try to answer the following code-related questions within the time \n limit. Keep in mind that incorrect answers will penalize your scoretime \n by ten seconds!", "h6"],
+    Q1 : [0, 9, 4, "Commonly used data types DO NOT include:","h2","subtext", "h6", " 1. strings ", " 2. boolean ", " 3. alerts ", " 4. numbers "],
+    Q2 : [0, 9, 4, "The condition in an if/else statement is enclosed within _____.","h2","subtext", "h6", " 1. quotes ", " 2. curly brackets ", " 3. parentheses ", " 4. square brackets "],
+    Q3 : [0, 10, 4, "Arrays in JavaScript can be used to store ______.","h2","subtext", "h6", " 1. numbers and strings ", " 2. other arrays ", " 3. bookmarks ", " 4. all of the above "],
+    Q4 : [0, 9, 4, "String values must be enclosed within_____ when being assigned to variables.","h2","subtext", "h6", " 1. commas ", " 2. curly brackets ", " 3. quotes ", " 4. parentheses "],
+    Q5 : [0, 10, 4, "A very useful tool used during development and debugging for printing content to the debugger is: ","h2","subtext", "h6", " 1. JavaScript ", " 2. terminal / bash ", " 3. for loops ", " 4. console.log "],
+    Q6 : [0, 0, 0, "All done!", "h2","Your final score is ", "."]
+
+};
+
+
+// function UpdateQuizQuestionsData {
+
+//     let 
+// }
+
+/* <script>
+function myFunction() {
+  document.getElementById("panel").style.display = "block";
+}
+</script> */
